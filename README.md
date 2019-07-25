27c41e514048f4adb25580708c304f80e0ad8b6a
Andrey <i@dru.su>
2019-07-25 23:55:16 +0700

    Docker compose

---
+++ README.md
@@ -0,0 +1,12 @@
+# Docker example
+
+To start server run:
+```
+$ yarn dev
+```
+
+
+To start docker-compose run:
+```
+$ docker-compose up
+```
--- index.js
+++ index.js
@@ -1,5 +1,41 @@
+const { Client } = require('pg')
+const { send } = require('micro')
+const { router, get, post } = require('microrouter')
+const url = require('url')
+const redirect = require('micro-redirect')
 
-module.exports = (req, res) => {
-	res.end('hello')
+const client = new Client({
+  user: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: 5432,
+  database: 'docker_test',
+  password: '123',
+})
+
+client.connect()
+
+const form = '<div><form action="/add"><input name=text><input type="submit"></form></div>'
+
+const index = async (req, res) => {
+    const result = await client.query('select * from messages')
+    const messages = result.rows && result.rows.map(row => `<div>${row.text}</div>`).join('')
+    const respPage = `<html><body>${form}${messages}</body></html>`
+    return respPage
 }
 
+const add = async (req, res) => {
+    const parts = url.parse(req.url, true)
+    const result = await client.query('insert into messages values ($1)', [parts.query.text])
+    redirect(res, 302, '/')
+}
+ 
+module.exports = router(
+    get('/', index), 
+    get('/add', add)
+)
+
+client.query("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE  table_schema = 'public' AND table_name = 'messages')", (err, result) => {
+    if (!result.rows[0].exists) {
+        client.query('create table messages (text varchar(255));')
+    }
+})
